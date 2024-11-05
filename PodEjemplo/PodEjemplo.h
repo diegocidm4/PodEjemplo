2ab449dce74473adcedb6e4c279a78426d69c0a6
Diego Cid Merino <diegocidm4@hotmail.com>
2024-11-05 22:36:24 +0100

    Initial project setup

---
+++ PodEjemplo/PodEjemplo.h
@@ -0,0 +1,18 @@
+//
+//  PodEjemplo.h
+//  PodEjemplo
+//
+//  Created by Diego Cid Merino on 5/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodEjemplo.
+FOUNDATION_EXPORT double PodEjemploVersionNumber;
+
+//! Project version string for PodEjemplo.
+FOUNDATION_EXPORT const unsigned char PodEjemploVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodEjemplo/PublicHeader.h>
+
+
---
+++ PodEjemploExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodEjemploExamples
+//
+//  Created by Diego Cid Merino on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
